59db763aa90b96523800a51212f8a84198d17576
AndreeaZiduu <andreeamaria.zidu@ulbsibiu.ro>
2026-04-27 18:26:29 +0300

    Laborator 7, ex 7.6.1 si 7.6.2

---
+++ src/main/java/ro/ulbs/proiectare/lab7/util/PasswordMaker.java
@@ -0,0 +1,39 @@
+package ro.ulbs.proiectare.lab7.util;
+
+public class PasswordMaker {
+    private static final int MAGIC_NUMBER = 3;
+    private static final String MAGIC_STRING;
+
+    private static PasswordMaker instance = null;
+    private static int contor = 0;
+
+    private String name;
+
+    static {
+        StringRandomizer srand = new StringRandomizer();
+        MAGIC_STRING = srand.randomString(20);
+
+        instance = new PasswordMaker("DefaultName");
+    }
+
+    private PasswordMaker(String name) {
+        this.name = name;
+    }
+
+    public static PasswordMaker getInstance(){
+        contor++;
+        return instance;
+    }
+    public static int getContor(){
+        return contor;
+    }
+
+    public String getPassword() {
+        java.util.Random r = new java.util.Random();
+        String ln = ""+name.length();
+        ln += r.nextInt(101);
+        StringRandomizer srand = new StringRandomizer();
+        return srand.randomString(MAGIC_NUMBER) + srand.randomString(10, MAGIC_STRING) + ln;
+    }
+}
+
